49e7f778b24108ce665960b924faa7265cce0d8f
vovikitsme <divo.82@gmail.com>
2025-04-30 16:02:21 +0300

    Create DataService.cs

---
+++ Services/DataService.cs
@@ -0,0 +1,54 @@
+﻿using Anno1800.Database;
+using Anno1800.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anno1800.Services
+{
+    public class DataService
+    {
+
+        private readonly ISQLiteAsyncConnection _db;
+
+        public DataService(SqliteConnectionFactory sqliteConnectionFactory)
+        {
+            _db = sqliteConnectionFactory.Connect();
+        }
+
+        public async Task<List<NeedDisplayDto>> GetNeedsByPopulationClassAsync(string populationClassName)
+        {
+            var populationClass = await _db.Table<PopulationClass>()
+                .FirstOrDefaultAsync(p => p.Name == populationClassName);
+
+            if (populationClass == null)
+                return new List<NeedDisplayDto>();
+
+            var needs = await _db.Table<Need>()
+                .Where(n => n.PopulationClassId == populationClass.Id)
+                .ToListAsync();
+
+            var needDtos = new List<NeedDisplayDto>();
+
+            foreach (var need in needs)
+            {
+                var needType = await _db.FindAsync<NeedType>(need.NeedTypeId);
+
+                needDtos.Add(new NeedDisplayDto
+                {
+                    Name = need.Name,
+                    IconPath = need.IconPath,
+                    NeedTypeName = needType?.Name ?? "?",
+                    PopulationClassName = populationClass.Name,
+                    ConsumptionRate = need.ConsumptionRate
+                });
+            }
+
+            return needDtos;
+        }
+    }
+}
